d57513808c03202cc82367e971daf84192e3c370
sw-yoonsung <sw.yoonsung@gmail.com>
2018-11-11 23:47:04 +0900

    Complete #3 steps

--- App.js
+++ App.js
@@ -1,12 +1,57 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import {StyleSheet, Text, View, StatusBar} from 'react-native';
+import Weather from './Weather';
 
-export default class App extends React.Component {
+const API_KEY = "863a05656efa837ad897d057c8c8923b";
+
+export default class App extends Component {
+  state = {
+    isLoaded: false,
+    error: null,
+    temperature: null,
+    name: null
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        console.log(position);
+        this.setState(
+          this._getWeather(position.coords.latitude, position.coords.longitude));
+        // isLoaded: true,
+        // error: "Something was wrong fucking!!"
+      },
+      error => {
+        console.log(error);
+        this.setState({
+          error: error
+        })
+      }
+    );
+  }
+
+  _getWeather = (lat, lon) => {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${API_KEY}`)
+      .then(response => response.json())
+      .then(json => {
+        console.log(json);
+        this.setState({
+          temperature:json.main.temp,
+          name: json.weather[0].main,
+          isLoaded: true
+        })
+      })
+  }
+
   render() {
+    const {isLoaded, error} = this.state;
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>This is Yoonsung!</Text>
+        <StatusBar hidden={true}/>
+        {isLoaded ? <Weather/> : <View style={styles.loading}>
+          <Text style={styles.loadingText}>Getting the fucking weather </Text>
+          {error ? <Text style={styles.errorText}>{error}</Text> : null}
+        </View>}
       </View>
     );
   }
@@ -16,7 +61,20 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  errorText: {
+    backgroundColor: 'transparent',
+    color: 'red',
+    marginBottom: 40
+  },
+  loading: {
+    flex: 1,
+    backgroundColor: '#FDF6AA',
+    justifyContent: 'flex-end',
+    paddingLeft: 24
+  },
+  loadingText: {
+    fontSize: 38,
+    marginBottom: 100,
+  }
 });
